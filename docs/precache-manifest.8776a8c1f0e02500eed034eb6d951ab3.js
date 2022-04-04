e97a2acfc270701a80456151e1f475db77871960
Silviu <sil.gherman@gmail.com>
2022-04-04 12:49:42 +0200

    Added build

---
+++ docs/precache-manifest.8776a8c1f0e02500eed034eb6d951ab3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9119b0c7b1f661aa62c439ad89d54d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82560c8068ceb0b10d2a",
+    "url": "/static/css/main.c9eb86bf.chunk.css"
+  },
+  {
+    "revision": "18f5c0fca64bea8c301b",
+    "url": "/static/js/2.b6901cab.chunk.js"
+  },
+  {
+    "revision": "cb643f225cdc3e16377ff69b77204a48",
+    "url": "/static/js/2.b6901cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82560c8068ceb0b10d2a",
+    "url": "/static/js/main.bcf0f7a4.chunk.js"
+  },
+  {
+    "revision": "ed8d8971ce09ea5fe1af",
+    "url": "/static/js/runtime-main.c3d5ff18.js"
+  },
+  {
+    "revision": "444cea35534af4a9609c903b97c2712f",
+    "url": "/static/media/dark-bubble.444cea35.png"
+  }
+]);
